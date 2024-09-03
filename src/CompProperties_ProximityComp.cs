79cbcadd5f1632fdb0e0878061cbcc140e64c0d0
Bry <pheonix2105@gmail.com>
2024-09-03 15:02:19 +0100

    Added mod folder

---
+++ src/CompProperties_ProximityComp.cs
@@ -0,0 +1,136 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using Verse;
+
+namespace BoUnderwater
+{
+    public class CompProperties_ProximityComp : CompProperties
+    {
+        public float ProximityTriggerDistance = 10f;
+    }
+
+    public class ProximityComp : ThingComp
+    {
+        private bool IsThingInProximity = false;
+        public CompProperties_ProximityComp Props => (CompProperties_ProximityComp)props;
+
+        public override void CompTick()
+        {
+            base.CompTick();
+            if (parent.IsHashIntervalTick(250))
+            {
+                IEnumerable<Pawn> PawnsInRange = GetPawnsInRange(this.parent.Position, this.parent.MapHeld, Props.ProximityTriggerDistance);
+
+                Log.Message($"Things near {PawnsInRange.Count()}"); 
+                if (PawnsInRange.Any())
+                {
+                    if (!IsThingInProximity)
+                    {
+                        OnProximityEnter(PawnsInRange.First());  
+                    }
+                }
+                else if (IsThingInProximity)
+                {
+                    OnProximityLeave();
+                }
+            }
+        }
+
+        public virtual void OnProximityEnter(Thing Thing)
+        {
+            IsThingInProximity = true;
+        }
+
+        public virtual void OnProximityLeave()
+        {
+            IsThingInProximity = false;
+        }
+
+        private IEnumerable<Pawn> GetPawnsInRange(IntVec3 Center, Map Map, float Radius)
+        {
+            return GenRadial.RadialCellsAround(Center, Radius, true)
+                .SelectMany(C => C.GetThingList(Map))
+                .OfType<Pawn>();
+        }
+
+        public override string CompInspectStringExtra()
+        {
+            return base.CompInspectStringExtra() + (IsThingInProximity ? "something in proximity" : "nothing near");
+        }
+    }
+
+    public class CompProperties_Proximity_ShowMote : CompProperties_ProximityComp
+    {
+        //public float ProximityTriggerDistance = 1f;
+        public EffecterDef EffectorToShow;
+
+        public CompProperties_Proximity_ShowMote()
+        {
+            compClass = typeof(Proximity_ShowMoteComp);
+        }
+    }
+
+    public class Proximity_ShowMoteComp : ProximityComp
+    {
+        public new CompProperties_Proximity_ShowMote Props => (CompProperties_Proximity_ShowMote)props;
+
+        private Effecter EffectorInstance;
+
+        public override void OnProximityEnter(Thing Thing)
+        {
+            base.OnProximityEnter(Thing);
+
+            CompGlower compGlower = this.parent.TryGetComp<CompGlower>();
+
+            if (compGlower != null)
+            {
+                compGlower.GlowColor = ColorInt.FromHdrColor(Color.red);
+            }
+
+
+            if (Props.EffectorToShow != null)
+            {
+
+                if (EffectorInstance != null)
+                {
+                    EffectorInstance.Cleanup();
+                    EffectorInstance = null;
+                }
+
+                EffectorInstance = Props.EffectorToShow.SpawnMaintained(this.parent.Position, this.parent.MapHeld);
+
+            }
+        }
+
+        public override void OnProximityLeave()
+        {
+            base.OnProximityLeave();
+
+            if (EffectorInstance != null)
+            {
+                EffectorInstance.Cleanup();
+                EffectorInstance = null;
+            }
+
+
+            CompGlower compGlower = this.parent.TryGetComp<CompGlower>();
+
+            if (compGlower != null)
+            {
+                compGlower.GlowColor = ColorInt.FromHdrColor(Color.green);
+            }
+
+        }
+
+        public override void CompTick()
+        {
+            base.CompTick();
+
+            if (EffectorInstance != null)
+            {
+                EffectorInstance.EffectTick(parent, parent);
+            }
+        }
+    }
+}
--- src/GameCondition_UnderWater.cs
+++ src/GameCondition_UnderWater.cs
@@ -13,10 +13,11 @@ namespace BoUnderwater
     public class GameCondition_UnderWaterCondition : GameCondition
     {
         private Color SkyColor = new Color(1f, 1f, 1f); // color of the sky
+        private Color SkyColorNight = Color.white;
         private Color ShadowColor = new Color(0.4f, 0, 0, 0.2f); // color of any shadows
         private Color OverlayColor = new Color(0.5f, 0.5f, 0.5f); // strength (opacity) of the sky color
         private float Saturation = 0.75f; // saturation of the sky color (0 = grayscale)
-        private float Glow = 1.25f; // strength of any actual light in each cell on the map
+        private float Glow = 1; // strength of any actual light in each cell on the map
 
         public override int TransitionTicks => 120; // how quickly the sky changes color (120 ticks = 2 seconds)
         public override void Init()
@@ -26,6 +27,7 @@ namespace BoUnderwater
             UnderWaterGameConditionDef def = (UnderWaterGameConditionDef)this.def;
 
             this.SkyColor = def.SkyColor;
+            this.SkyColorNight = def.SkyColorNight;
             this.ShadowColor = def.ShadowColor;
             this.OverlayColor = def.OverlayColor;
             this.Saturation = def.SkyColorSaturation;
@@ -37,9 +39,15 @@ namespace BoUnderwater
             return GameConditionUtility.LerpInOutValue(this, TransitionTicks);
         }
 
-        public SkyColorSet TestSkyColors =>
-           new SkyColorSet(SkyColor, ShadowColor, OverlayColor, Saturation);
-
+        public SkyColorSet TestSkyColors
+        {
+            get
+            {
+                float dayPercent = GenCelestial.CurCelestialSunGlow(Find.CurrentMap);
+                Color lerpedColor = Color.Lerp(SkyColorNight, SkyColor, dayPercent);
+                return new SkyColorSet(lerpedColor, ShadowColor, OverlayColor, Saturation);
+            }
+        }
         public override SkyTarget? SkyTarget(Map map)
         {
             return new SkyTarget(Glow, TestSkyColors, 1f, 1f);
@@ -50,6 +58,7 @@ namespace BoUnderwater
     public class UnderWaterGameConditionDef : GameConditionDef
     {
         public Color SkyColor;
+        public Color SkyColorNight;
         public Color ShadowColor;
         public Color OverlayColor;
 
@@ -61,4 +70,23 @@ namespace BoUnderwater
             conditionClass = typeof(GameCondition_UnderWaterCondition);
         }
     }
+
+        
+        //    <BoUnderwater.UnderWaterGameConditionDef>
+        //      <defName>UnderWaterEnvironment</defName>
+        //      <label>Underwater Environment</label>
+        //      <description>An underwater environment with unique visual effects.</description>
+        //      <conditionClass>BoUnderwater.GameCondition_UnderWaterCondition</conditionClass>
+        //      <SkyColor>(0.1, 0.1, 0.1)</SkyColor>
+        //      <SkyColorNight>(0.4, 0.4, 0.5)</SkyColorNight>
+        //      <ShadowColor>(1, 0, 0)</ShadowColor>
+        //      <OverlayColor>(1, 1, 1, 1)</OverlayColor>
+        //      <SkyColorSaturation>0.8</SkyColorSaturation>
+        //      <OverallGlowIntensityMultiplier>1</OverallGlowIntensityMultiplier>
+        //      <letterDef>NegativeEvent</letterDef>
+        //      <weatherDef>Clear</weatherDef>
+        //      <canBePermanent>true</canBePermanent>
+        //      <natural>false</natural>
+        //      <allowUnderground>true</allowUnderground>
+        //    </BoUnderwater.UnderWaterGameConditionDef>
 }
